a210c32b11b13a90fff6eaeb7a672b81f4ff6252
Mostafa <--add>
2020-05-13 15:48:55 +0200

    edit title

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Nextjs API routes</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -19,9 +19,6 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-
-       
-
 
         .title {
           margin: 0;
